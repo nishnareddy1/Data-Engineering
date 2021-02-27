a22efb50eb9e3b51afce490e4ff6a5d01f9b0917
nishnareddy1 <aletinishna0723@gmail.com>
2021-02-26 23:29:36 -0800

    data integration activity

--- Data Integration/part_D_Analysis.py
+++ Data Integration/part_D_Analysis.py
@@ -1,35 +1,32 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-a = ('cases', 'Poverty')
-b = ('deaths', 'Poverty')
-c = ('cases', 'IncomePerCap')
-d = ('deaths', 'IncomePerCap')
-e = ('Dec_Cases', 'Poverty')
-f = ('Dec_Cases', 'Poverty')
-g = ('Dec_Cases', 'IncomePerCap')
-h = ('Dec_Cases', 'IncomePerCap')
+lis = []
+one = ('cases', 'Poverty')
+lis.append(one)
+two = ('deaths', 'Poverty')
+lis.append(two)
+three = ('cases', 'IncomePerCap')
+lis.append(three)
+four = ('deaths', 'IncomePerCap')
+lis.append(four)
+five = ('Dec_Cases', 'Poverty')
+lis.append(five)
+six = ('Dec_Cases', 'Poverty')
+lis.append(six)
+seven = ('Dec_Cases', 'IncomePerCap')
+lis.append(seven)
+eight = ('Dec_Cases', 'IncomePerCap')
+lis.append(eight)
 
 oregondf = df.loc[df.index.get_level_values('state') == 'Oregon']
 
-cor = []
-
-cor.append(a)
-cor.append(b)
-cor.append(c)
-cor.append(d)
-cor.append(e)
-cor.append(f)
-cor.append(g)
-cor.append(h)
-
 # For Oregon Data
-plots_oregon = []
-for row in cor:
+oregon = []
+for row in lis:
     graph = (oregondf[row[0]].corr(df[row[1]]), row[0], row[1])
-    plots_oregon.append(graph)
+    oregon.append(graph)
 
-for i in plots_oregon:
+for i in oregon:
     R = i[0]
     print("R values", R)
     if (R > 0.5 or R < -0.5):
@@ -37,12 +34,12 @@ for i in plots_oregon:
         sns.scatterplot(data=oregondf, x=i[1], y=i[2])
 
 # USA
-plots_all=[]
-for row in cor:
-    graph=(df[row[0]].corr(df[row[1]]),row[0],row[1])
-    plots_all.append(graph)
+all=[]
+for row in lis:
+    R=(df[row[0]].lis(df[row[1]]),row[0],row[1])
+    all.append(R)
 
-for i in plots_all:
+for i in all:
     R=i[0]
     print("R values", R)
     if(R>0.5 or R<-0.5):
